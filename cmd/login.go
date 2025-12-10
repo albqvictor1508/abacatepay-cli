70cd7cf517d1dd30e5ed3f4cbc22a61804209f8f
Victor Albuquerque <albq.victor@gmail.com>
2025-12-10 09:27:30 -0300

    wip(login): interactive mode and field validation on cmd

--- cmd/login.go
+++ cmd/login.go
@@ -1,6 +1,7 @@
 package cmd
 
 import (
+	"fmt"
 	"os/exec"
 
 	"github.com/albqvictor1508/abacatepay-cli/internal/prompts"
@@ -21,8 +22,8 @@ var loginCmd = &cobra.Command{
 }
 
 func init() {
-	loginCmd.Flags().StringVar(&apiKey, "key", "", "API Key do Abacate Pay")
-	loginCmd.Flags().StringVar(&name, "name", "", "Nome do profile (mín 3, máx 50 caracteres)")
+	loginCmd.Flags().StringVar(&apiKey, "key", "", "Abacate Pay's API Key")
+	loginCmd.Flags().StringVar(&name, "name", "", "Profile name (min 3, max 50 characters)")
 
 	rootCmd.AddCommand(loginCmd)
 }
@@ -31,7 +32,13 @@ func loginOnAbacatepay() error {
 	var cmd *exec.Cmd
 
 	if apiKey == "" || name == "" {
-		if err := prompts.
+		if err := prompts.InteractiveMode(apiKey); err != nil {
+			return err
+		}
+	}
+
+	if len(name) < 3 || len(name) > 50 {
+		return fmt.Errorf("the profile name must to be at 3 and 50 characters")
 	}
 
 	return cmd.Start()
