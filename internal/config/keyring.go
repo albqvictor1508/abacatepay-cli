bdbc8b03e1c4147877db91ca91b2a98b2a573de0
Victor Albuquerque <albq.victor@gmail.com>
2025-12-10 10:15:03 -0300

    chore(config): create keyring operations

--- internal/config/keyring.go
+++ internal/config/keyring.go
@@ -1,4 +1,19 @@
 package config
 
-func Salve() {
+import (
+	keyring "github.com/zalando/go-keyring"
+)
+
+var service = "abacate"
+
+func SaveToKeyring(name, apiKey string) error {
+	return keyring.Set(service, name, apiKey)
+}
+
+func GetFromKeyring(name string) (string, error) {
+	return keyring.Get(service, name)
+}
+
+func DeleteFromKeyring(name string) error {
+	return keyring.Delete(service, name)
 }
