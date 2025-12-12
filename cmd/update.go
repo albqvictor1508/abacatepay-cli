572d9c87d666c86b661628a66790e3caa44ea3a6
Victor Albuquerque <albq.victor@gmail.com>
2025-12-12 18:34:19 -0300

    chore(update): update command created

--- cmd/update.go
+++ cmd/update.go
@@ -1,7 +1,9 @@
 package cmd
 
 import (
+	"fmt"
 	"log"
+	"os"
 
 	"github.com/blang/semver"
 	"github.com/rhysd/go-github-selfupdate/selfupdate"
@@ -14,7 +16,8 @@ var updateCmd = &cobra.Command{
 	Use:   "update",
 	Short: "Update Abacatepy CLI when new version is available",
 	RunE: func(cmd *cobra.Command, args []string) error {
-		return update()
+		update()
+		return nil
 	},
 }
 
@@ -22,11 +25,35 @@ func init() {
 	rootCmd.AddCommand(updateCmd)
 }
 
-func update() error {
+func update() {
 	latest, err := CheckForUpdate()
 	if err != nil {
 		log.Println("error checking for update: %w", err)
+		return
 	}
+
+	if latest == nil {
+		fmt.Println("Current version is the latest") // TODO: trocar pelo surveys
+		return
+	}
+
+	exe, err := os.Executable()
+	if err != nil {
+		log.Println("Could not locate executable path")
+		return
+	}
+
+	if err := selfupdate.UpdateTo(latest.AssetURL, exe); err != nil {
+		if os.IsPermission(err) {
+			log.Println("permission denied, please run the update command with sudo: sudo abacate update")
+			return
+		}
+
+		log.Println("error ocurred while updating binary:", err)
+		return
+	}
+	log.Println("Successfully updated to version", latest.Version)
+	return
 }
 
 func CheckForUpdate() (*selfupdate.Release, error) {
@@ -45,4 +72,12 @@ func CheckForUpdate() (*selfupdate.Release, error) {
 }
 
 func ShowUpdate(version string) {
+	latest, err := CheckForUpdate()
+
+	if err != nil || latest == nil {
+		return
+	}
+
+	// TODO: colocar um popup aq, tipo "Update Available" pra quando o usuario
+	// iniciar a CLI aparecer
 }
